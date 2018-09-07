b9ecd754ef6fa60099b67d589eb4b8d0ca39d239
Diego Enriquez <diegoenap@gmail.com>
2018-09-07 18:03:50 +1200

    R markdown files for data visualization divided by physical visitors and GA metrics for each location.

--- asianart_analysis.R
+++ asianart_analysis.R
@@ -7,6 +7,7 @@ source("asianart_func.R")
 # Read Visitors data ====
 
 asianart_visitors <- readVisitorData()
+asianart_visitors <- asianart_visitors[Date >= "2016-01-01"]
 head(asianart_visitors)
 summary(asianart_visitors)
 str(asianart_visitors)
@@ -60,3 +61,56 @@ ga_visit_chn_os <- readQueryExplorerData("data/ga/asian_visit_chn_os.tsv", c("Da
 ga_visit_chn_us <- readQueryExplorerData("data/ga/asian_visit_chn_us.tsv", c("Date", "Channel"))
 ga_visit_chn_do <- readQueryExplorerData("data/ga/asian_visit_chn_do.tsv", c("Date", "Channel"))
 ga_visit_chn_lo <- readQueryExplorerData("data/ga/asian_visit_chn_lo.tsv", c("Date", "Channel"))
+
+
+# Create weekend dataframe ====
+
+daysinfo <- data.table(Date = seq(as.Date("2016-01-01"), as.Date("2018-06-30"), by = "day"))
+daysinfo$weekNum <- wday(daysinfo$Date, week_start = 1)
+daysinfo$isFirstSunday <- wday(daysinfo$Date, week_start = 1) == 7 & day(daysinfo$Date) <= 7
+
+
+# Exploring Visitors ====
+
+ggplot(asianart_visitors, aes(Date, visitors)) +
+  geom_line(stat = "identity", size = 0.25) +
+  geom_vline(xintercept = weekends[isWeekend == TRUE, Date], col = "lightblue", lty = 2) +
+  theme_bw()
+
+ggplot(asianart_visitors, aes(Date, visitors)) +
+  geom_line(stat = "identity", size = 0.25) +
+  theme_bw() +
+  geom_vline(xintercept = weekends[isWeekend == TRUE, Date], col = "lightblue", lty = 2) +
+  coord_cartesian(xlim = as.Date(c("2017-01-01", "2017-12-31")), ylim = c(0, 5000))
+
+
+
+
+ggplot(asianart_visitors, aes(Date, visitors)) +
+  geom_line(size = 0.5, col = "grey") +
+  geom_line(data = movingAverage(asianart_visitors), aes(Date, visitors), size = 0.5, col = "blue") +
+  theme_bw() +
+  labs(title = "Asian Art Museum visits - Weekends") +
+  geom_vline(xintercept = weekends[isWeekend == TRUE, Date], col = "lightblue", lty = 2) +
+  coord_cartesian(xlim = as.Date(c("2017-06-01", "2017-07-31")), ylim = c(0, 5000))
+
+ggplot(asianart_visitors, aes(Date, visitors)) +
+  geom_line(size = 0.5, col = "grey") +
+  geom_point(size = 0.75, col = "grey15") +
+  geom_line(data = movingAverage(asianart_visitors), aes(Date, visitors), size = 0.5, col = "blue") +
+  theme_bw() +
+  labs(title = "Asian Art Museum visits - Weekends and Thursdays") +
+  geom_vline(xintercept = weekends[isThursday == TRUE, Date], col = "indianred2", lty = 2) +
+  geom_vline(xintercept = weekends[isWeekend == TRUE, Date], col = "lightblue", lty = 2) +
+  coord_cartesian(xlim = as.Date(c("2017-09-01", "2017-10-31")), ylim = c(0, 5000))
+
+ggplot(asianart_visitors, aes(Date, visitors)) +
+  geom_line(size = 0.5, col = "grey") +
+  geom_point(size = 0.5, col = "grey15") +
+  #geom_line(data = movingAverage(asianart_visitors), aes(Date, visitors), size = 0.5, col = "blue") +
+  geom_line(data = trendValues(asianart_visitors), aes(Date, visitors), size = 0.5, col = "blue") +
+  theme_bw() +
+  labs(title = "Asian Art Museum visits - Months") +
+  coord_cartesian(xlim = as.Date(c("2017-01-01", "2017-12-31")), ylim = c(0, 5000))
+
+
--- asianart_func.R
+++ asianart_func.R
@@ -13,6 +13,7 @@ readVisitorData <- function(addDateCols = FALSE, beonicfile = NULL) {
   return(data)
 }
 
+
 readQueryExplorerData <- function(filename, colNames, addDateCols = FALSE) {
   # AsianArt GA (from Google Query Explorer)
   # Join all the files in one dataset
@@ -40,6 +41,7 @@ readQueryExplorerData <- function(filename, colNames, addDateCols = FALSE) {
   return(data)
 }
 
+
 completeGADates <- function(data, limitDates = NULL, replaceNA = 0) {
   # Complete the dataset with the missing date so it contains all the dates within the range
   if (is.null(limitDates))
@@ -61,4 +63,98 @@ completeGADates <- function(data, limitDates = NULL, replaceNA = 0) {
   data[is.na(SessionsBounce)]$SessionsBounce <- replaceNA
   data[is.na(SessionsNoBounce)]$SessionsNoBounce <- replaceNA
   return(data)
+}
+
+
+movingAverage <- function(data, w = 7) {
+  # Calculates the moving average of the variables in the dataset, given the window (w).
+  # Returns a new data.table with the dates and moving averages
+  # The returning dataset will have w-1 rows
+  if (nrow(data) >= w) {
+    data <- data[order(Date)]
+    n <- nrow(data)
+    # Get the structure of the dataset
+    if ("visitors" %in% colnames(data))
+      tmpdf <- data[1, .(Date, visitors)]
+    else
+      tmpdf <- data[1, .(Date, Users, UsersOld, Sessions, SessionsNew, SessionsOld, SessionsBounce, SessionsNoBounce, AvgSessionDuration, Pageviews, PageviewsSession, UniquePageviews, AvgTimeOnPage)]
+    # Compute moving average
+    for (i in 1:(n - w + 1)) {
+      if ("visitors" %in% colnames(data))  # Select columns depending on dataset
+        mv_values <- apply(data[i:(i + w - 1), .(visitors)], 2, mean)
+      else
+        mv_values <- apply(data[i:(i + w - 1), .(Users, UsersOld, Sessions, SessionsNew, SessionsOld, SessionsBounce, SessionsNoBounce, AvgSessionDuration, Pageviews, PageviewsSession, UniquePageviews, AvgTimeOnPage)], 2, mean)
+      tmprow <- data.frame(Date = data[i + w - 1, Date], t(mv_values))
+      tmpdf <- rbind(tmpdf, tmprow)
+    }
+    return(tmpdf[-1])
+  } else {
+    stop("Moving average window should be greater or equal to the number of rows")
+  }
+}
+
+
+trendValues <- function(data) {
+  # Replace the metric's values with the trend
+  data <- data[order(Date)]
+  if ("visitors" %in% colnames(data)) {
+    data$visitors = as.numeric(stl(ts(data$visitors, frequency = 7), s.window = "periodic")$time.series[,2])
+  } else {
+    data$Sessions = as.numeric(stl(ts(data$Sessions, frequency = 7), s.window = "periodic")$time.series[,2])
+    data$SessionsNew = as.numeric(stl(ts(data$SessionsNew, frequency = 7), s.window = "periodic")$time.series[,2])
+    data$SessionsOld = as.numeric(stl(ts(data$SessionsOld, frequency = 7), s.window = "periodic")$time.series[,2])
+    data$SessionsBounce = as.numeric(stl(ts(data$SessionsBounce, frequency = 7), s.window = "periodic")$time.series[,2])
+    data$SessionsNoBounce = as.numeric(stl(ts(data$SessionsNoBounce, frequency = 7), s.window = "periodic")$time.series[,2])
+    data$AvgSessionDuration = as.numeric(stl(ts(data$AvgSessionDuration, frequency = 7), s.window = "periodic")$time.series[,2])
+    data$Users = as.numeric(stl(ts(data$Users, frequency = 7), s.window = "periodic")$time.series[,2])
+    data$UsersOld = as.numeric(stl(ts(data$UsersOld, frequency = 7), s.window = "periodic")$time.series[,2])
+    data$Pageviews = as.numeric(stl(ts(data$Pageviews, frequency = 7), s.window = "periodic")$time.series[,2])
+    data$PageviewsSession = as.numeric(stl(ts(data$PageviewsSession, frequency = 7), s.window = "periodic")$time.series[,2])
+    data$UniquePageviews = as.numeric(stl(ts(data$UniquePageviews, frequency = 7), s.window = "periodic")$time.series[,2])
+    data$AvgTimeOnPage = as.numeric(stl(ts(data$AvgTimeOnPage, frequency = 7), s.window = "periodic")$time.series[,2])
+  }
+  return(data)
+}
+
+
+# Plot functions ====
+
+getPlotGA <- function(gadata, gametric, holidays = NULL, showTrend = TRUE, showSH = FALSE, showWE = FALSE, showPH = FALSE, showCS = FALSE, showE = FALSE, ylimits = NULL, logData = FALSE, impDates = TRUE) {
+  #xlablimits <- as.Date(c("2015-07-01", "2018-05-31"))
+  xlablimits <- as.Date(c("2016-07-06", "2018-06-30"))
+  ylabel <- ""
+  if (logData) {
+    gadata <- logValues(gadata, impDates = impDates)
+    ylabel <- "(log)"
+  } 
+  p <- ggplot(gadata, aes_string("Date", gametric)) +
+    scale_x_date(limits = xlablimits) +
+    theme_bw()
+  if (!is.null(ylimits)) p <- p + scale_y_continuous(limits = ylimits)
+  if (showSH) p <- p + geom_vline(xintercept = holidays[SchoolHoliday == 1, Date], col = "grey90")
+  if (showWE) p <- p + geom_vline(xintercept = holidays[IsWeekend == 1, Date], col = "lightgrey", lty = 3)
+  if (showPH) p <- p + geom_vline(xintercept = holidays[PublicHoliday == 1, Date], col = "lightgrey")
+  if (showCS) p <- p + geom_vline(xintercept = holidays[CruiseShip == 1, Date], col = "lightgrey")
+  if (showE) p <- p + geom_vline(xintercept = holidays[Event == 1, Date], col = "lightgrey")
+  p <- p + geom_line(col = "grey90")
+  if (showTrend) p <- p + geom_line(data = movingAverage(gadata), aes_string("Date", gametric), col = "blue")
+  p <- p + labs(x = "", y = paste(gametric, ylabel))
+  return(p)
+}
+
+getPlotGAList <- function(data, holidays = NULL, showTrend = TRUE, logData = FALSE, impDates = TRUE) {
+  p <- vector("list", 12)
+  p[[1]] <- getPlotGA(data, "Sessions", holidays, showTrend = showTrend, showSH = FALSE, logData = logData, impDates = impDates)
+  p[[2]] <- getPlotGA(data, "SessionsNew", holidays, showTrend = showTrend, showSH = FALSE, logData = logData, impDates = impDates)
+  p[[3]] <- getPlotGA(data, "SessionsOld", holidays, showTrend = showTrend, showSH = FALSE, logData = logData, impDates = impDates)
+  p[[4]] <- getPlotGA(data, "SessionsBounce", holidays, showTrend = showTrend, showSH = FALSE, logData = logData, impDates = impDates)
+  p[[5]] <- getPlotGA(data, "SessionsNoBounce", holidays, showTrend = showTrend, showSH = FALSE, logData = logData, impDates = impDates)
+  p[[6]] <- getPlotGA(data, "AvgSessionDuration", holidays, showTrend = showTrend, showSH = FALSE, logData = logData, impDates = impDates)
+  p[[7]] <- getPlotGA(data, "Users", holidays, showTrend = showTrend, showSH = FALSE, logData = logData, impDates = impDates)
+  p[[8]] <- getPlotGA(data, "UsersOld", holidays, showTrend = showTrend, showSH = FALSE, logData = logData, impDates = impDates)
+  p[[9]] <- getPlotGA(data, "Pageviews", holidays, showTrend = showTrend, showSH = FALSE, logData = logData, impDates = impDates)
+  p[[10]] <- getPlotGA(data, "PageviewsSession", holidays, showTrend = showTrend, showSH = FALSE, logData = logData, impDates = impDates)
+  p[[11]] <- getPlotGA(data, "UniquePageviews", holidays, showTrend = showTrend, showSH = FALSE, logData = logData, impDates = impDates)
+  p[[12]] <- getPlotGA(data, "AvgTimeOnPage", holidays, showTrend = showTrend, showSH = FALSE, logData = logData, impDates = impDates)
+  return(p)
 }
